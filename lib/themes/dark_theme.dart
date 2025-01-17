52361f97f01a73bf919f2ed90decfcae9593a593
Oudomsak PHABOUDY <oudomsak5566@gmail.com>
2025-01-17 18:31:28 +0700

    ...

--- lib/themes/dark_theme.dart
+++ lib/themes/dark_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 final ThemeData darkTheme = ThemeData.light().copyWith(
+  scaffoldBackgroundColor: const Color(0xFFFFBF7F6),
   primaryColor: Colors.teal,
   appBarTheme: AppBarTheme(
     color: const Color(0xFFFBD021),
--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData.light().copyWith(
+  scaffoldBackgroundColor: const Color(0xFFFFBF7F6),
   primaryColor: const Color(0xFFEC1C29),
   appBarTheme: AppBarTheme(
     color: const Color(0xFFEC1C29),
